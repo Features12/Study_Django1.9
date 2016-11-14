d6eba81dfc3bf60f2f160b50e9e8824ad1659ad0
Features <Features.inna@mail.ru>
2016-11-15 00:59:37 +0300

    add $ in regex

--- app_buy/shop_app/urls.py
+++ app_buy/shop_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import output, garbage, items_details, login, reg
 
 urlpatterns = [
-    url(r'^', output),#main page
+    url(r'^$', output),#main page
     url(r'^garbage/$', garbage), #garbage
     url(r'^items_details/$', items_details), #info items
     url(r'^login/$', login), # login
