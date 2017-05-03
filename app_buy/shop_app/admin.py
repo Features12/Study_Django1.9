9d7bac6dc5ac287508ac2fe5fe436169496bd698
Inna_Features <features.inna@mail.ru>
2017-05-03 12:26:30 +0300

    del app_buy

--- app_buy/shop_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Shop_List, Shop_Cart
-
-admin.site.register(Shop_List)
-admin.site.register(Shop_Cart)
-
--- app_buy/shop_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopAppConfig(AppConfig):
-    name = 'shop_app'
